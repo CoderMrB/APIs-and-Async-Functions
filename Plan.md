2abb2b643b888d4ae1a7dd4af4075b6d065a253c
Chris Branson <chrisbranson@live.co.uk>
2023-07-20 14:07:22 +0100

    Separated functions into different HTML pages and attached sepearate js

---
+++ Plan.md
@@ -0,0 +1,8 @@
+Features to implment:
+1 - Landing page
+2 -Random game picker
+3 - Find a selection of games for a certain number of players
+4 - Find a selection of games by category eg strategy, card, coop
+5 - Booking system - show days available that week
+6 - Record of games played - data, game, winner, comments
+7 - Leaderboard
---
+++ randomiser.js
@@ -0,0 +1,18 @@
+let gameOptions = ["scythe", "nemesis", "wingspan", "carcassonne"]
+let randomiser = document.getElementById("randomiser")
+randomiser.addEventListener('click', getRandomGame)
+
+let randomiserDiv =document.getElementById("randomiserSection")
+
+let randomImage = document.createElement('img')
+randomImage.setAttribute('id', 'randomImage')
+
+async function getRandomGame(){
+    randomiserDiv.append(randomImage)
+    randomImage.setAttribute('src', './rollingDie.gif')
+    let randomGame = Math.floor(Math.random()*(gameOptions.length))
+    console.log(gameOptions[randomGame])
+    let gameData = await fetch (`https://api.boardgameatlas.com/api/search?name=${gameOptions[randomGame]}&client_id=DwheEZXOaI`)
+    let gameDataResult = await gameData.json()
+    randomImage.setAttribute('src', gameDataResult.games[0].image_url)
+}
