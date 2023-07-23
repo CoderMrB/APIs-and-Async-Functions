4da54d3ae5a8e63b59ec6c13697e4c7df3076579
Chris Branson <chrisbranson@live.co.uk>
2023-07-23 16:35:11 +0100

    added function to calculate number of wins for each player

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -2,11 +2,36 @@ const SUPABASE_URL = 'https://gllewomldkmnizscnkdw.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsbGV3b21sZGttbml6c2Nua2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5MzkyNDYsImV4cCI6MjAwNTUxNTI0Nn0.9EJ3gR0wQoqLDMSfM3NI617lFhH7VosZ41xSChpy7BI';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
+let ChrisScore = 0
+let MarkScore = 0
+let NatScore = 0
+let MikeScore = 0
+let PhilScore = 0
+
 async function fetchData() {
   let { data: History, error } = await _supabase
   .from('History')
   .select('WinnerName')
   console.log(History)
+  for(let i=0;i<History.length; i++){
+    switch(History[i].WinnerName) {
+      case 'Chris':
+        ChrisScore ++
+        break;
+      case "Mark":
+        MarkScore ++
+        break;
+      case "Nat":
+        NatScore ++
+        break;
+      case "Phil":
+        PhilScore ++
+        break;
+      case "Mike":
+        MikeScore ++
+        break;
+    }
+  }console.log(`Chris has won ${ChrisScore} times.\n Mark has won ${MarkScore} times.`)
 }
 
 // Call the async function
