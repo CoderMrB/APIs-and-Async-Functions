a94ff865457e49a0558c410a437e371c6742610c
Chris Branson <chrisbranson@live.co.uk>
2023-07-23 17:28:46 +0100

    function to update scores in object

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -2,7 +2,16 @@ const SUPABASE_URL = 'https://gllewomldkmnizscnkdw.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsbGV3b21sZGttbml6c2Nua2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5MzkyNDYsImV4cCI6MjAwNTUxNTI0Nn0.9EJ3gR0wQoqLDMSfM3NI617lFhH7VosZ41xSChpy7BI';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-let Scores =  {ChrisScore: 0, MarkScore: 0, NatScore: 0, MikeScore: 0, PhilScore: 0}
+let Scores =  [{Name: Chris, Score:0}, {Name:Mark, Score: 0}, {Name:Nat, Score:0}, {Name: Mike, Score: 0}, {Name: Phil, Score: 0}]
+
+function compare( a, b ) {
+  if ( a.last_nom < b.last_nom ){
+    return -1;
+  }
+  if ( a.last_nom > b.last_nom ){
+    return 1;
+  }
+  return 0;
 
 async function fetchData() {
   const { data: History, error } = await _supabase
@@ -10,25 +19,19 @@ async function fetchData() {
   .select('WinnerName')
   console.log(History)
   for(let i=0;i<History.length; i++){
-    switch(History[i].WinnerName) {
-      case 'Chris':
-        Scores.ChrisScore ++
-        break;
-      case "Mark":
-        Scores.MarkScore ++
-        break;
-      case "Nat":
-        Scores.NatScore ++
-        break;
-      case "Phil":
-        Scores.PhilScore ++
-        break;
-      case "Mike":
-        Scores.MikeScore ++
-        break;
+    for(let x=0; x<score.length; x++){
+      if (History.WinnerName[i] === Scores[x].Name){
+        Scores[x].Score ++
+      }
     }
-  }console.log(`Chris has won ${Scores.ChrisScore} times.\n Mark has won ${Scores.MarkScore} times.`)
-}
+    }console.log(`Chris has won ${Scores.ChrisScore} times.\n Mark has won ${Scores.MarkScore} times.`)
+    console.log(Scores)
+  }
+
+  
+  }
+  
+
 
 // Call the async function
 fetchData();
