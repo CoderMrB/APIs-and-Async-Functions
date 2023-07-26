6d13e25915964b7e2166c292bcc44fd27d7c19d8
Chris Branson <chrisbranson@live.co.uk>
2023-07-26 14:25:58 +0100

    Changed search functionality to search by number of players and play time

--- public/lookUpGame.js
+++ public/lookUpGame.js
@@ -1,23 +1,59 @@
-let searchBar = document.getElementById("search")
-let searchValue = " "
-searchBar.addEventListener('input', updateSearchValue)
-let submitButton = document.getElementById("submit")
-submitButton.addEventListener('click', retrieveGameData)
+let searchPlayers = document.getElementById("players")
+let searchPlayersValue = 0
+searchPlayers.addEventListener('input', updateSearchPlayersValue)
 
-let gameImage = document.createElement('img')
-gameImage.setAttribute('id', 'gameImage')
+let searchTime = document.getElementById("time")
+let searchTimeValue = 0
+searchTime.addEventListener('input', updateSearchPlayersValue)
+
+
+let submitButton = document.getElementById("submit")
+submitButton.addEventListener('click', get_all_data)
+
+let gameNameDisplay = document.getElementById("gameName")
+let gameImage = document.getElementById("gameImage")
+
 
 let homebody = document.getElementById("home")
 
-function updateSearchValue(){
-    searchValue = searchBar.value
-    console.log(searchValue)
+function updateSearchNameValue(){
+    searchNameValue = searchName.value
+    console.log(searchNameValue)
 }
 
-async function retrieveGameData(){
-    let gameData = await fetch (`https://api.boardgameatlas.com/api/search?name=${searchValue}&client_id=DwheEZXOaI`)
+function updateSearchPlayersValue(){
+    searchPlayersValue = searchPlayers.value
+    console.log(searchPlayersValue)
+}
+
+async function retrieveGameData(page_number){
+    // let gameData = await fetch (`https://api.boardgameatlas.com/api/search?name=${searchNameValue}&client_id=DwheEZXOaI`)
+    // let gameDataResult = await gameData.json()
+    // console.log(gameDataResult)
+    // gameImage.setAttribute('src', gameDataResult.games[0].image_url)
+    // homebody.append(gameImage)
+
+    let gameData = await fetch (`https://api.boardgameatlas.com/api/search?min_players=${parseInt(searchPlayersValue)}&max-playtime<=${searchTimeValue}&limit=100&client_id=DwheEZXOaI`)
     let gameDataResult = await gameData.json()
-    console.log(gameDataResult)
-    gameImage.setAttribute('src', gameDataResult.games[0].image_url)
-    homebody.append(gameImage)
+    const filteredGames = gameDataResult.games.filter(game => game.min_players !== null && game.max_playtime !== null);
+    return filteredGames
+}
+
+
+
+
+async function get_all_data() {
+    const total_results_response = await fetch(`https://api.boardgameatlas.com/api/search?min_players=${parseInt(searchPlayersValue)}&min-playtime<=${searchTimeValue}&limit=100&client_id=DwheEZXOaI`);
+    const total_results_data = await total_results_response.json();
+    const total_results = total_results_data.count;
+    const total_pages = Math.ceil(total_results / 100);
+    let page = Math.floor(Math.random()*total_pages)
+    const pageData = await retrieveGameData(page);
+    let chosenGame = pageData[Math.floor(Math.random()*100)]
+    console.log(chosenGame)
+
+    
+    gameImage.setAttribute('src', chosenGame.image_url)
+    gameNameDisplay.innerHTML = `${chosenGame.name} can be played with ${chosenGame.min_players} people and shouldn't take longer than ${chosenGame.max_playtime} minutes to complete` 
+
 }
