7ac2cb2a3e10d7f59885aeafdd356c9c61ae11fd
Chris Branson <chrisbranson@live.co.uk>
2023-07-26 21:17:07 +0100

    Finalised functions for finding new games

--- public/lookUpGame.js
+++ public/lookUpGame.js
@@ -1,10 +1,10 @@
 let searchPlayers = document.getElementById("players")
-let searchPlayersValue = 0
-searchPlayers.addEventListener('input', updateSearchPlayersValue)
+// let searchPlayersValue = 0
+// searchPlayers.addEventListener('input', updateSearchPlayersValue)
 
 let searchTime = document.getElementById("time")
-let searchTimeValue = 0
-searchTime.addEventListener('input', updateSearchPlayersValue)
+// let searchTimeValue = 0
+// searchTime.addEventListener('input', updateSearchPlayersValue)
 
 
 let submitButton = document.getElementById("submit")
@@ -16,15 +16,15 @@ let gameImage = document.getElementById("gameImage")
 
 let homebody = document.getElementById("home")
 
-function updateSearchNameValue(){
-    searchNameValue = searchName.value
-    console.log(searchNameValue)
-}
+// function updateSearchNameValue(){
+//     searchNameValue = searchName.value
+//     console.log(searchNameValue)
+// }
 
-function updateSearchPlayersValue(){
-    searchPlayersValue = searchPlayers.value
-    console.log(searchPlayersValue)
-}
+// function updateSearchPlayersValue(){
+//     searchPlayersValue = searchPlayers.value
+//     console.log(searchPlayersValue)
+// }
 
 async function retrieveGameData(page_number){
     // let gameData = await fetch (`https://api.boardgameatlas.com/api/search?name=${searchNameValue}&client_id=DwheEZXOaI`)
@@ -33,17 +33,17 @@ async function retrieveGameData(page_number){
     // gameImage.setAttribute('src', gameDataResult.games[0].image_url)
     // homebody.append(gameImage)
 
-    let gameData = await fetch (`https://api.boardgameatlas.com/api/search?min_players=${parseInt(searchPlayersValue)}&max-playtime<=${searchTimeValue}&limit=100&client_id=DwheEZXOaI`)
+    let gameData = await fetch (`https://api.boardgameatlas.com/api/search?min_players=${searchPlayers.value}&max_playtime=${searchTime.value}&max_playtime_operator=lt&limit=100&client_id=DwheEZXOaI`)
     let gameDataResult = await gameData.json()
     const filteredGames = gameDataResult.games.filter(game => game.min_players !== null && game.max_playtime !== null);
     return filteredGames
 }
 
 
-
-
 async function get_all_data() {
-    const total_results_response = await fetch(`https://api.boardgameatlas.com/api/search?min_players=${parseInt(searchPlayersValue)}&min-playtime<=${searchTimeValue}&limit=100&client_id=DwheEZXOaI`);
+    console.log(`number of players: ${searchPlayers.value}\n amount of time: ${searchTime.value}`)
+    gameImage.setAttribute('src', './rollingDie.gif')
+    const total_results_response = await fetch(`https://api.boardgameatlas.com/api/search?min_players=${searchPlayers.value}&max_playtime=${searchTime.value}&max_playtime_operator=lt&limit=100&client_id=DwheEZXOaI`);
     const total_results_data = await total_results_response.json();
     const total_results = total_results_data.count;
     const total_pages = Math.ceil(total_results / 100);
@@ -51,9 +51,6 @@ async function get_all_data() {
     const pageData = await retrieveGameData(page);
     let chosenGame = pageData[Math.floor(Math.random()*100)]
     console.log(chosenGame)
-
-    
     gameImage.setAttribute('src', chosenGame.image_url)
-    gameNameDisplay.innerHTML = `${chosenGame.name} can be played with ${chosenGame.min_players} people and shouldn't take longer than ${chosenGame.max_playtime} minutes to complete` 
-
+    gameNameDisplay.innerHTML = `<strong>${chosenGame.name} </strong> can be played with <strong> ${chosenGame.min_players} people </strong> and should take no longer than <strong> ${chosenGame.max_playtime} minutes </strong> to complete.` 
 }
